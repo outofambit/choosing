7ab1dc4f037b2474a735811024c3bd9ba6d83d3f
Evelyn Masso <evoutofambit@gmail.com>
2016-03-17 13:32:12 -0700

    init

---
+++ compile.sh
@@ -0,0 +1,1 @@
+browserify -t [ babelify --presets [ react babel-preset-es2015 ] ] main.js -o public/scripts/bundle.js
---
+++ main.js
@@ -0,0 +1,72 @@
+
+let React = require('react');
+let ReactDOM = require('react-dom');
+
+const Switch = (props) =>
+  <button key={props.label} className={props.active ? 'active' : 'inactive'} onClick={() => {props.action(); props.parentAction(props.label)}}>
+    {props.label}
+  </button>;
+
+class SwitchSet extends React.Component {
+  constructor(props) {
+    super(props);
+
+    var kvs = this.props.switchData.map((da) => {
+      return [da.label, false];
+    });
+    let stateMap = new Map(kvs);
+    this.state = {
+      switches: stateMap
+    };
+  }
+
+  handleSwitchClicked (label) {
+    let newSwitchMap = new Map();
+    this.state.switches.forEach((v, k, o) =>
+      newSwitchMap.set(k, (k == label))
+    )
+    // set state of switches
+    this.setState({switches: newSwitchMap});
+  }
+
+  render () {
+    let sws = this.props.switchData.map(function(da) {
+      return (
+        <Switch {...da} active={this.state.switches.get(da.label)} parentAction={this.handleSwitchClicked.bind(this)} key={da.label} />
+      );
+    }.bind(this));
+    return (
+      <div className="switchSet">
+        {sws}
+      </div>
+    );
+  }
+  
+}
+
+var swda = {
+  switchData:
+    [
+      {
+        label: 'hello',
+        action: () => {console.log('hello!')}
+      },
+      {
+        label: 'goodbye',
+        action: () => {console.log('goodbye!')}
+      },
+      {
+        label: 'stay',
+        action: () => {console.log('staying!')}
+      },
+      {
+        label: 'chinta',
+        action: () => {console.log('chinta!')}
+      },
+    ]
+};
+
+ReactDOM.render(
+  <SwitchSet {...swda} />,
+  document.getElementById('react')
+);
