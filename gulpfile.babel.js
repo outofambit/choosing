f7cc8e7a9d9b6536c10b4fa06b548957f2a3810b
Evelyn Masso <evoutofambit@gmail.com>
2016-03-17 17:55:32 -0700

    gulp for build

---
+++ gulpfile.babel.js
@@ -0,0 +1,16 @@
+
+var gulp = require('gulp')
+var browserify = require('browserify')
+var babelify = require('babelify')
+var source = require('vinyl-source-stream')
+
+gulp.task('build', () => {
+  var b = browserify('main.js')
+  b.transform(babelify, {presets: ['es2015', 'react']})
+
+  return b.bundle().pipe(source('bundle.js')).pipe(gulp.dest('public/scripts'))
+})
+
+gulp.task('default', () => {
+  // place code for your default task here
+});
