e655ec8b0daec18a20f74f0806f94cd89bf536a2
Evelyn Masso <evoutofambit@gmail.com>
2016-03-26 12:33:49 -0700

    add sass

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,14 +3,24 @@ var gulp = require('gulp')
 var browserify = require('browserify')
 var babelify = require('babelify')
 var source = require('vinyl-source-stream')
+var sass = require('gulp-sass');
 
-gulp.task('build', () => {
+gulp.task('js', () => {
   var b = browserify('src/main.js')
   b.transform(babelify, {presets: ['es2015', 'react']})
 
   return b.bundle().pipe(source('bundle.js')).pipe(gulp.dest('public/scripts'))
 })
 
-gulp.task('dev', () => gulp.watch('src/main.js', ['build']))
+gulp.task('sass', () => {
+  return gulp.src('src/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('public/css'));
+});
 
-gulp.task('default', ['build', 'dev']);
+gulp.task('dev', () => {
+  gulp.watch('src/main.js', ['js'])
+  gulp.watch('src/*.scss', ['sass'])
+})
+
+gulp.task('default', ['sass', 'js', 'dev']);
