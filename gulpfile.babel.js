d845aadcb65e4d6a9900851e09cb9d987589b9ea
Evelyn Masso <evoutofambit@gmail.com>
2016-03-26 11:59:04 -0700

    a little resturcturing

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,15 +5,12 @@ var babelify = require('babelify')
 var source = require('vinyl-source-stream')
 
 gulp.task('build', () => {
-  var b = browserify('main.js')
+  var b = browserify('src/main.js')
   b.transform(babelify, {presets: ['es2015', 'react']})
 
   return b.bundle().pipe(source('bundle.js')).pipe(gulp.dest('public/scripts'))
 })
 
-gulp.task('dev', () => gulp.watch('main.js', ['build']))
+gulp.task('dev', () => gulp.watch('src/main.js', ['build']))
 
-
-gulp.task('default', () => {
-  // place code for your default task here
-});
+gulp.task('default', ['build', 'dev']);
